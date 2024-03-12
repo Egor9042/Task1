2ab5482ccab8942e6db664d2e39b9b6a5b439997
Egor Pokrovsky <egor.pokrovsky2000@gmail.com>
2024-03-12 20:54:52 +0400

    Sem2

---
+++ OneDrive/Рабочий стол/Lesson1/Sem2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и удаляет вторую цифру этого числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+int result = firstDigit * 10 + thirdDigit;
+
+Console.WriteLine(result);
