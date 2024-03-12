8afc3c388ac9d234e81a02fe572504fab7d198ad
Egor Pokrovsky <egor.pokrovsky2000@gmail.com>
2024-03-12 22:04:50 +0400

    Sem2_4

---
+++ OneDrive/Рабочий стол/Lesson1/Sem2_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью слева
+// цифру заданного числа или сообщает, что третьей
+// цифры нет.
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+    num = num * -1;
+}
+
+if (num < 100)
+{
+    Console.WriteLine("Третей цифры нет");
+}
+else
+{
+    while (num > 999)
+    {
+    num = num / 10;
+    }
+    Console.WriteLine(num % 10);
+}
